8f16321fea97d364c1f3ef7ea273acadbfd6ed50
halfalpine <rocksandwoks@gmail.com>
2016-04-14 09:52:42 -0400

    Add rudimentary display_board method

--- intermediate ruby/ttt2.rb
+++ intermediate ruby/ttt2.rb
@@ -4,14 +4,35 @@
     Game < TTT
 
     def initialize
-      empty_board = "1|2|3\n4|5|6\n7|8|9"
+      @empty_board = "1|2|3\n4|5|6\n7|8|9"
+      @changeable_board = (0..9).to_a
       turns
     end
 
     def turns
-      puts empty_board
+      puts "Available spaces:"
+      puts @empty_board
+      puts ""
+      puts "Current board:"
+      display_board
+    end
+
+    def display_board
+      board_readout = ""
+      @changeable_board[1..9].each do |square|
+        if square.is_a? Numeric
+          board_readout += " "
+        else board_readout += square
+        end
+
+      end
+      puts board_readout
     end
     
+  end
+
+  class Player
+
   end
 end
 
