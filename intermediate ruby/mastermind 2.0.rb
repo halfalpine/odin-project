05427e18dc3f3f0757dc0bd9851136dd516fb801
halfalpine <rocksandwoks@gmail.com>
2016-04-29 20:01:45 -0400

    Add process_input and user_generated_code methods

--- intermediate ruby/mastermind 2.0.rb
+++ intermediate ruby/mastermind 2.0.rb
@@ -14,31 +14,65 @@ class Game
   include Rules
 
   def hack
-    puts @answer
+    puts "Hi"
   end
 
   def initialize
     puts Rules.give_rules
     @answer = []
+    @turns = 12
     pick_sides
   end
 
   def pick_sides
-    puts "Press ENTER to create a code. Press any key + ENTER to guess the code."
+    puts "Press any key + ENTER to guess the code. Press ENTER to create the code."
     @input = gets.chomp
     quit?
-    @input == "" ? user_generated_code : computer_generated_code
+    @input == "" ? computer_generated_code : user_generated_code
+  end
+
+  def computer_turn
   end
 
   def turn 
   end
 
   def user_generated_code
+    prompt_input
+    process_input
+    check_input
+    computer_turn
+  end
+
+  def prompt_input
+    puts "\nEnter four colors. Choose from RED, BLUE, YELLOW, GREEN, WHITE, or BLACK."
+    @input = gets.chomp
+  end
+
+  def process_input
+    @processed_input = []
+    @raw_input.upcase.split.each do |word| 
+      word.gsub(/[,]/, "")
+      @processed_input.push(word[0..2].upcase)
+    end
+    @processed_input
+  end
+
+  def check_input
+    check_length
+    check_colors
+  end
+
+  def check_length
+    prompt_input unless @processed_input.length == 4
+  end
+
+  def check_colors
+    prompt_input unless (@processed_input - COLORS).empty?
   end
 
   def computer_generated_code
     @answer = Rules::COLORS.shuffle.values_at(0..3)
-    hack
     turn
   end
 
